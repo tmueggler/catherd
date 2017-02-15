e72de69985235cf5ed5de0c4fc37d62b47cb5747
Thomas Mueggler <thomas.mueggler@gmail.com>
2017-02-15 16:30:40 +0100

    Basic handcrafted ng2 app setup

---
+++ frontend/src/systemjs.config.js
@@ -0,0 +1,24 @@
+(function (global) {
+    SystemJS.config({
+        paths: {
+            'npm:': 'node_modules/'
+        },
+        map: {
+            app: 'app',
+            '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+            '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+            '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+            '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+            '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+            rxjs: 'npm:rxjs'
+        },
+        packages: {
+            app: {
+                defaultExtensions: '*.js'
+            },
+            rxjs: {
+                defaultExtensions: '*.js'
+            }
+        }
+    });
+})(this);
