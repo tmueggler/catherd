4051fe31b17c748478aafde28ef3ce44882a7539
Thomas Mueggler <thomas.mueggler@gmail.com>
2017-02-22 23:00:05 +0100

    Added index.ts for single export, import Using rollup to bundle api in single file

---
+++ api/rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+    entry: 'dist/@catherd/api/index.js',
+    format: 'umd',
+    moduleName: 'catherd.api',
+    sourceMap: true,
+    dest: 'dist/@catherd/api/bundles/api.umd.js'
+};
