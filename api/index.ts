c09d1c22c3abfc9a3a06f6cad19196bd8396ae86
Thomas Mueggler <thomas.mueggler@gmail.com>
2017-03-09 23:20:46 +0100

    Expose authorization messages

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+export * from './src/auth/authorization.messages';
 export * from './src/control/control.messages';
 export * from './src/gateway/gateway.model';
 export * from './src/state/state.messages';
