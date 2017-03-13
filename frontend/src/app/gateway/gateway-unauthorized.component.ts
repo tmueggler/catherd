77857f8ff1e165f632bbca14ec83d4ba7981f75c
Thomas Mueggler <thomas.mueggler@gmail.com>
2017-03-13 14:00:09 +0100

    Handle failure

--- frontend/src/app/gateway/gateway-unauthorized.component.ts
+++ frontend/src/app/gateway/gateway-unauthorized.component.ts
@@ -1,25 +1,31 @@
 import {Component, OnInit} from "@angular/core";
 import {Gateway} from "@catherd/api/web";
 import {GatewayRepo} from "./gateway.repo";
+import {NotificationService} from "../notification.service";
 
 @Component({
     selector: 'gateways-unauthorized',
     templateUrl: 'app/gateway/gateway-unauthorized.component.html'
 })
 export class GatewayUnauthorizedComponent implements OnInit {
-    constructor(private readonly gatewayrepo: GatewayRepo) {
+    constructor(private readonly gatewayrepo: GatewayRepo, private readonly notification: NotificationService) {
     }
 
     gateways: Gateway.Info[] = [];
 
     ngOnInit(): void {
         this.gatewayrepo.unauthorized()
-            .subscribe((res) => this.gateways = res);
+            .subscribe(
+                (res) => this.gateways = res,
+                (error) => console.warn(`Failed to load unauthorized gateways. Reason ${error}`) // TODO show in view
+            );
     }
 
     authorize(trg: Gateway.Info) {
         this.gatewayrepo.authorize(trg)
-            .subscribe((res) => { // TODO show success
-            });
+            .subscribe(
+                (res) => this.notification.success('_gateway_authorize_success_'),
+                (error) => this.notification.warning('_gateway_authorize_failed_')
+            );
     }
 }
