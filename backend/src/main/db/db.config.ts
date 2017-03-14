907dc1a55861d951ae1923f9cde4ace6dbeeff1c
Thomas Mueggler <thomas.mueggler@gmail.com>
2017-03-14 11:09:13 +0100

    Removed obsolete primary key. Using default 'id'.

--- backend/src/main/db/db.config.ts
+++ backend/src/main/db/db.config.ts
@@ -2,4 +2,3 @@ export const HOST: string = '192.168.56.102';
 export const PORT: number = 28015;
 export const DB_NAME: string = 'catherd';
 export const TABLE_GATEWAY: string = 'gateway';
-export const TABLE_GATEWAY_PK: string = 'uuid';
